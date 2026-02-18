f0f6f7ba5ce460f8347f250be9030b7a56a27f1b
Unai75 <unai.crupas@educa.jcyl.es>
2026-02-18 12:49:53 +0100

    Mejoro codigo

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,20 @@
-import java.util.List;
 import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         Tienda tienda = new Tienda();
 
-        // Llamamos al metodo cargarDesdeCSV para cargar la lista desde un archivo .csv
+        // Cargamos desde el CSV
         try {
             tienda.cargarDesdeCSV();
             System.out.println("Inventario cargado correctamente.");
         } catch (Exception e) {
             System.out.println("Aviso: No se encontró el archivo de stock. La tienda está vacía.");
         }
-
-        //Creamos esta variable para saber cuando salir del bucle while
+//Creamos esta variable para salir del bucle
         boolean salir = false;
         System.out.println("!Bienvenido a la tienda de electronica¡");
-        //Creamos un menu cada uno haciendo una cosa
 
         while (!salir) {
             System.out.println("---------------------------");
@@ -28,49 +23,32 @@ public class Main {
             System.out.println("3-Buscar producto");
             System.out.println("4-Finalizar compra y sacar factura");
             System.out.println("---------------------------");
+
+
             int opcion = sc.nextInt();
 
             switch (opcion) {
                 case 1:
-                    //Aqui ponemos que la lista de la tienda es la del metodo obtenerCatalogo de tienda
+                    // Muestra el catálogo
                     tienda.obtenerCatalogo();
                     break;
                 case 2:
-                    //En este caso pedimos él, id y la cantidad del producto después llamando al método añadirAlCarrito
-                    //Comprobando que si no da problemas si se puede añadir al carrito esos productos
-                    System.out.println("Dime el id del producto");
-                    int id = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println("Dime la cantidad del producto");
-                    int cantidad = sc.nextInt();
-                    sc.nextLine();
-                    boolean exito = tienda.anhadirAlCarrito(id, cantidad);
-                    if (exito) {
-                        System.out.println("El producto se ha añadido al carro sin problemas");
-                    } else {
-                        System.out.println("Ha habido un error añadiendo el producto");
-                    }
+                    //Añade al carrito lo que encontramos
+                    tienda.anhadirAlCarrito();
                     break;
                 case 3:
-                    System.out.println("Dime el id del producto");
-                    int idBuscar = sc.nextInt();
-                    Producto productoEncontrado = tienda.buscarProducto(idBuscar);
-                    //Si el producto no es null es que se ha encontrado
-                    if (productoEncontrado != null) {
-                        System.out.println("El producto se ha encontrado!" + productoEncontrado);
-                    } else {
-                        System.out.println("El producto con ese id no existe");
-                    }
+                    // Buscamos el producto por el id llamando al método de tienda
+                    tienda.buscarProducto();
                     break;
                 case 4:
-                    //En este método llamamos a generarFactura
+                    //Generamos la factura
                     tienda.generarFactura();
                     System.out.println("La factura ha sido generada en el archivo llamado facturas.txt, !Muchas gracias por su visita¡");
                     salir = true;
                     break;
-
+                default:
+                    System.out.println("Opción no válida");
             }
         }
-
     }
 }
--- src/Tienda.java
+++ src/Tienda.java
@@ -1,43 +1,69 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Tienda {
-    private final List<Producto> catalogo;
-    private  final List<Producto> carrito;
+    Scanner sc = new Scanner(System.in);
+    //Creamos las dos listas
+     List<Producto> catalogo = new ArrayList<>();
+    List<Producto> carrito = new ArrayList<>();
 
 
 
+    //Metodo para añadir objetos al carrito
+    public void anhadirAlCarrito() {
+        System.out.println("--- COMPRAR PRODUCTO ---");
 
-    //Se inician aquí las lista, ya que si no nos dará la excepcion NullPointerException
-    public Tienda() {
-        this.catalogo = new ArrayList<>();
-        this.carrito = new ArrayList<>();
-    }
 
-    //Añade al carrito los productos que se quiera añadir
-    public boolean anhadirAlCarrito(int id, int cantidadSolicitada) {
-        Producto p = buscarProducto(id);
+        System.out.println("Dime el id del producto:");
+        int id = sc.nextInt();
+        sc.nextLine();
 
+
+        Producto p = buscarProductoInterno(id);
+
+        //Salimos del metodo al no encontrar ningun producto con ese id
+        if (p == null) {
+            System.out.println(" Error: No existe ningún producto con este ID " + id);
+            return;
+        }
+
+
+        System.out.println("Has seleccionado: " + p.getNombre());
+        System.out.println("Dime la cantidad que quieres:");
+        int cantidadSolicitada = sc.nextInt();
+        sc.nextLine();
+
+        // En este metodo llamos al metodo restarStock si hay suficiente stock lo añadimos y si no lo hay nos lanza un mensaje de que no hay stock
         if (!p.restarStock(cantidadSolicitada)) {
-            System.out.println("No hay suficiente stock");
-            return false;
-        }
-        for (int i = 0; i < cantidadSolicitada; i++) {
-            carrito.add(p);
-        }
-        return true;
-    }
-
-    //Recorre el catálogo entero y muestra su información
-    public void obtenerCatalogo() {
-        for (Producto catalogo : this.catalogo) {
-            System.out.println(catalogo);
+            System.out.println("No hay suficiente stock. Solo quedan " + p.getStock());
+        } else {
+            for (int i = 0; i < cantidadSolicitada; i++) {
+                carrito.add(p);
+            }
+            System.out.println("El producto se ha añadido al carrito sin problemas.");
         }
     }
 
-    //busca información dependiendo del ID si no encuentra ningún producto dice que no se ha encontrado nada
-    public Producto buscarProducto(int id) {
+//Nos deja buscar el producto por id
+    public void buscarProducto() {
+        System.out.println("Dime el id del producto a buscar:");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        Producto p = buscarProductoInterno(id);
+
+        if (p != null) {
+            System.out.println("¡El producto se ha encontrado!");
+            System.out.println(p);
+        } else {
+            System.out.println("El producto con ese id no existe.");
+        }
+    }
+
+    //Metodo para buscar producto por id para no tener que escribir el bucle cada vez que queramos buscar por id
+    public Producto buscarProductoInterno(int id) {
         for (Producto producto : this.catalogo) {
             if (producto.getId() == id) {
                 return producto;
@@ -45,95 +71,85 @@ public class Tienda {
         }
         return null;
     }
-//En este metodo obtenemos los datos desde un csv
+//Nos muestra el catálogo
+    public void obtenerCatalogo() {
+        System.out.println("--- CATÁLOGO DISPONIBLE ---");
+        if (catalogo.isEmpty()) {
+            System.out.println("El catálogo está vacío.");
+        }
+        for (Producto p : this.catalogo) {
+            System.out.println(p);
+        }
+    }
+//Metodo para cargar desde CSV separando los datos haciendo con un split con ; para luego meterlo en un array
     public void cargarDesdeCSV() {
-
         try (BufferedReader br = new BufferedReader(new FileReader("stock.csv"))) {
-
             String linea;
-
             while ((linea = br.readLine()) != null) {
-
-
                 String[] datos = linea.split(";");
 
-                //Separamos los datos en huecos de un array separados por puntos y comas cada dato ocupando un espacio en el array
                 String tipo = datos[0];
                 int id = Integer.parseInt(datos[1]);
                 String nombre = datos[2];
                 String marca = datos[3];
                 double precio = Double.parseDouble(datos[4]);
                 int stock = Integer.parseInt(datos[5]);
+                //Dependiendo si es movil auricular o portatil añade las variables de cada clase
 
-                // Aquí comprobamos que si es móvil también se añada las variables que tiene el móvil
                 if (tipo.equalsIgnoreCase("Movil")) {
-
                     int bateria = Integer.parseInt(datos[6]);
                     int camaraMPX = Integer.parseInt(datos[7]);
-
-                    // Creamos el objeto de movil y lo metemos al catálogo
                     Producto p = new Movil(id, nombre, marca, precio, stock, bateria, camaraMPX);
                     this.catalogo.add(p);
 
-                    //Aquí se comprueba que si el tipo de Producto es un portátil se usa se añade los dos datos que quedan
-
                 } else if (tipo.equalsIgnoreCase("Portatil")) {
-                    // Sacamos los datos exclusivos de Portátil
                     int ram = Integer.parseInt(datos[6]);
                     String procesador = datos[7];
-
-                    // Creamos el objeto y lo metemos al catálogo
                     Producto p = new Portatil(id, nombre, marca, precio, stock, ram, procesador);
                     this.catalogo.add(p);
+
                 } else if (tipo.equalsIgnoreCase("Auricular")) {
                     String tipoDeConexion = datos[6];
                     Producto p = new Auricular(id, nombre, marca, precio, stock, tipoDeConexion);
-                    catalogo.add(p);
+                    this.catalogo.add(p);
                 }
             }
             System.out.println("Carga de datos completada con éxito.");
 
         } catch (IOException e) {
-
             System.out.println("Error leyendo el archivo CSV: " + e.getMessage());
         } catch (NumberFormatException e) {
-
             System.out.println("Error de formato en el CSV  " + e.getMessage());
         }
-
     }
-
+//Este metodo genera la factura con los objetos que se ha comprado
     public void generarFactura() {
-
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("facturas.txt"))) {
             bw.write("-----------Factura de la compra----------------");
             bw.newLine();
             double precioTotal = 0;
-            for (Producto p : this.carrito) {
 
-                //Ponemos que la variable del precio de cada producto sea el metodo de calcularPrecio de cada objeto
+
+
+            for (Producto p : this.carrito) {
+                //Creamos esta variable para que la variable sea el resultado de calcularPrecioFinal
                 double precioProducto = p.calcularPrecioFinal();
 
-                //Añadimos los datos al archivo
                 bw.write(p.getNombre() + " (" + p.getMarca() + ")");
                 bw.write(" ........................ ");
                 bw.write(precioProducto + " €");
-
-
                 bw.newLine();
 
-
-                // Sumamos al precio total
                 precioTotal += precioProducto;
-
             }
-            bw.write("El precio total de todos los productos comprados es " + precioTotal + " €");
-            bw.newLine();
             bw.write("-------------------------------------------");
+            bw.newLine();
+            bw.write("El precio total de todos los productos comprados es " + precioTotal + " €");
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
 
